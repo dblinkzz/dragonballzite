b70c40e925fc318ea6aa7a193f0522a902205348
dblinkzz <juancarlos.martinezcarrillo@outlook.com>
2016-05-22 19:08:24 -0600

    committing index.html file and pictures

---
+++ files/mc.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+/*
+     FILE ARCHIVED ON 7:03:33 Oct 10, 2002 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 0:46:07 May 23, 2016.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+var ap = window.navigator.appVersion.toLowerCase();
+var i = ap.indexOf("msie");
+if (ap.indexOf("windows") >= 0 && i >= 0 && ((i + 5) < ap.length)) {
+  var v = ap.charAt(i + 5);
+  if (v >= 5 ) {
+    document.write('<scr' + 'ipt src="/web/20021010070333/http://us.i1.yimg.com/us.yimg.com/i/mc/mc1.js"></scr' + 'ipt>');
+    document.write('<scr' + 'ipt src="/web/20021010070333/http://us.i1.yimg.com/us.yimg.com/i/mc/mc2.js"></scr' + 'ipt>');
+  }
+}
+
